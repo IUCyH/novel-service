016ed905ddb66328a243b33c7a875c69394a53c8
Jaemin <ghdckdwn8456@gmail.com>
2025-08-25 16:17:12 +0900

    [feat] NovelRepository title 기준 exists 체크 메서드 정의 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/repository/novel/NovelRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/repository/novel/NovelRepository.java
@@ -4,4 +4,6 @@ import com.iucyh.novelservice.domain.novel.Novel;
 import com.iucyh.novelservice.repository.BaseRepository;
 
 public interface NovelRepository extends BaseRepository<Novel, Long> {
+
+    boolean existsByTitle(String title);
 }
