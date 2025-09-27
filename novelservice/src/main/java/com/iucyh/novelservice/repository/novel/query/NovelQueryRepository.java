c24662b8d5a4bc39afb0e72368e8910d634ab3fa
Jaemin <ghdckdwn8456@gmail.com>
2025-09-27 15:22:45 +0900

    [chore] 변경된 구조에 맞게 리포지토리 코드 수정 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/repository/novel/query/NovelQueryRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/repository/novel/query/NovelQueryRepository.java
@@ -2,16 +2,31 @@ package com.iucyh.novelservice.repository.novel.query;
 
 import com.iucyh.novelservice.domain.novel.NovelCategory;
 import com.iucyh.novelservice.dto.novel.query.NovelPagingQueryDto;
+import com.iucyh.novelservice.repository.novel.query.cursor.NovelCursor;
+import com.iucyh.novelservice.repository.novel.query.pagingquery.NovelPagingQuery;
 
 import java.util.List;
 
 public interface NovelQueryRepository {
 
-    List<NovelPagingQueryDto> findNovels(NovelSearchCondition condition, int limit);
-    List<NovelPagingQueryDto> findNovelsByCategory(NovelSearchCondition condition, NovelCategory category, int limit);
+    <T extends NovelCursor, R extends NovelPagingQueryDto> List<R> findNovels(
+            NovelSearchCondition<T> condition,
+            NovelPagingQuery<T, R> pagingQuery,
+            int limit
+    );
+    <T extends NovelCursor, R extends NovelPagingQueryDto> List<R> findNovelsByCategory(
+            NovelSearchCondition<T> condition,
+            NovelPagingQuery<T, R> pagingQuery,
+            NovelCategory category,
+            int limit
+    );
 
     /**
      * 이번달 신작 소설 조회 메서드
      */
-    List<NovelPagingQueryDto> findNewNovels(NovelSearchCondition condition, int limit);
+    <T extends NovelCursor, R extends NovelPagingQueryDto> List<R> findNewNovels(
+            NovelSearchCondition<T> condition,
+            NovelPagingQuery<T, R> pagingQuery,
+            int limit
+    );
 }
--- novelservice/src/main/java/com/iucyh/novelservice/repository/novel/query/NovelQueryRepositoryImpl.java
+++ novelservice/src/main/java/com/iucyh/novelservice/repository/novel/query/NovelQueryRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.iucyh.novelservice.repository.novel.query;
 import com.iucyh.novelservice.domain.novel.NovelCategory;
 import com.iucyh.novelservice.dto.novel.query.NovelPagingQueryDto;
 import com.iucyh.novelservice.repository.novel.query.cursor.NovelCursor;
+import com.iucyh.novelservice.repository.novel.query.cursor.NovelPopularCursor;
+import com.iucyh.novelservice.repository.novel.query.pagingquery.NovelPagingQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -20,18 +22,27 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<NovelPagingQueryDto> findNovels(NovelSearchCondition condition, int limit) {
-        JPAQuery<NovelPagingQueryDto> query = createPagingQuery(condition.getCursor());
-        return query
+    public <T extends NovelCursor, R extends NovelPagingQueryDto> List<R> findNovels(
+            NovelSearchCondition<T> condition,
+            NovelPagingQuery<T, R> pagingQuery,
+            int limit
+    ) {
+        return pagingQuery
+                .createQuery(queryFactory, condition.getCursor())
                 .where(novel.deletedAt.isNull())
                 .limit(limit)
                 .fetch();
     }
 
     @Override
-    public List<NovelPagingQueryDto> findNovelsByCategory(NovelSearchCondition condition, NovelCategory category, int limit) {
-        JPAQuery<NovelPagingQueryDto> query = createPagingQuery(condition.getCursor());
-        return query
+    public <T extends NovelCursor, R extends NovelPagingQueryDto> List<R> findNovelsByCategory(
+            NovelSearchCondition<T> condition,
+            NovelPagingQuery<T, R> pagingQuery,
+            NovelCategory category,
+            int limit
+    ) {
+        return pagingQuery
+                .createQuery(queryFactory, condition.getCursor())
                 .where(
                         novel.deletedAt.isNull(),
                         novel.category.eq(category)
@@ -41,20 +52,20 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
     }
 
     @Override
-    public List<NovelPagingQueryDto> findNewNovels(NovelSearchCondition condition, int limit) {
+    public <T extends NovelCursor, R extends NovelPagingQueryDto> List<R> findNewNovels(
+            NovelSearchCondition<T> condition,
+            NovelPagingQuery<T, R> pagingQuery,
+            int limit
+    ) {
         LocalDateTime thisMonth = getThisMonth();
-        JPAQuery<NovelPagingQueryDto> query = createPagingQuery(condition.getCursor());
-        return query
+        return pagingQuery
+                .createQuery(queryFactory, condition.getCursor())
                 .where(
                         novel.deletedAt.isNull(),
                         novel.lastEpisodeAt.goe(thisMonth)
                 )
                 .limit(limit)
                 .fetch();
-    }
-
-    private JPAQuery<NovelPagingQueryDto> createPagingQuery(NovelCursor cursor) {
-        return cursor.createPagingQuery(queryFactory);
     }
 
     private LocalDateTime getThisMonth() {
