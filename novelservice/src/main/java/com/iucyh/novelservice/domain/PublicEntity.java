909d65c69c4447e1d56b178d84262f9ee670de56
Jaemin <ghdckdwn8456@gmail.com>
2025-10-03 18:00:56 +0900

    [feat] public id -> UUID 타입으로 변경 및 uuid v7 기반으로 변경, BINARY(16) 타입으로 컬럼 설정 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/domain/PublicEntity.java
+++ novelservice/src/main/java/com/iucyh/novelservice/domain/PublicEntity.java
@@ -1,5 +1,6 @@
 package com.iucyh.novelservice.domain;
 
+import com.fasterxml.uuid.Generators;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -10,6 +11,9 @@ import java.util.UUID;
 @Getter
 public abstract class PublicEntity extends DateEntity {
 
-    @Column(nullable = false, unique = true, updatable = false, length = 32)
-    private String publicId = UUID.randomUUID().toString().replace("-", "");
+    @Column(
+            columnDefinition = "BINARY(16)",
+            unique = true, nullable = false, updatable = false
+    )
+    private UUID publicId = Generators.timeBasedEpochGenerator().generate();
 }
