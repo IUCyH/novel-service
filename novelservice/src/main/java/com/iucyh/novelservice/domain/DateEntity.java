f3cf7c2afd783c43bb1ada1d847e49c1a66d59b2
Jaemin <ghdckdwn8456@gmail.com>
2025-10-03 17:56:40 +0900

    [feat] Auditing -> Hibernate 기반에서 Spring Data JPA 기반으로 변경 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/domain/DateEntity.java
+++ novelservice/src/main/java/com/iucyh/novelservice/domain/DateEntity.java
@@ -1,23 +1,25 @@
 package com.iucyh.novelservice.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class DateEntity {
 
-    @CreationTimestamp
+    @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
-    @Column(nullable = false)
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     private LocalDateTime deletedAt;
