0ecd58075049d0b0731871805e24976c24c87d5b
Jaemin <ghdckdwn8456@gmail.com>
2025-10-09 04:28:31 +0900

    [chore] NovelQueryService -> 공통 쿼리 실행 메서드에서 dto 필드 꺼내는 순서 조정 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/service/novel/NovelQueryService.java
+++ novelservice/src/main/java/com/iucyh/novelservice/service/novel/NovelQueryService.java
@@ -100,8 +100,8 @@ public class NovelQueryService {
             NovelPagingRequestDto pagingRequest,
             BiFunction<NovelSearchCondition, NovelPagingQuery, List<? extends NovelPagingQueryDto>> queryFunc
     ) {
-        String cursor = pagingRequest.getCursor();
         NovelSortType sortType = NovelSortType.of(pagingRequest.getSort());
+        String cursor = pagingRequest.getCursor();
 
         NovelPagingQuery pagingQuery = getPagingQuery(sortType);
         NovelSearchCondition searchCondition = createSearchCondition(sortType, cursor);
