d90fafff3f5e8ebffdfe652bab33293757891924
Jaemin <ghdckdwn8456@gmail.com>
2025-10-13 23:37:10 +0900

    [fix] 소설 조회 공통 조건 추가, 신작 조회 기준 -> 생성일 기준으로 변경 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/repository/novel/query/NovelQueryRepositoryImpl.java
+++ novelservice/src/main/java/com/iucyh/novelservice/repository/novel/query/NovelQueryRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.iucyh.novelservice.dto.novel.query.NovelPagingQueryDto;
 import com.iucyh.novelservice.repository.novel.query.cursor.NovelCursor;
 import com.iucyh.novelservice.repository.novel.query.cursor.NovelPopularCursor;
 import com.iucyh.novelservice.repository.novel.query.pagingquery.NovelPagingQuery;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +26,7 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
     public List<? extends NovelPagingQueryDto> findNovels(NovelSearchCondition condition, NovelPagingQuery pagingQuery, int limit) {
         return pagingQuery
                 .createQuery(queryFactory, condition.getCursor())
-                .where(novel.deletedAt.isNull())
+                .where(getDefaultFilterCondition())
                 .limit(limit)
                 .fetch();
     }
@@ -35,7 +36,7 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
         return pagingQuery
                 .createQuery(queryFactory, condition.getCursor())
                 .where(
-                        novel.deletedAt.isNull(),
+                        getDefaultFilterCondition(),
                         novel.category.eq(category)
                 )
                 .limit(limit)
@@ -48,11 +49,16 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
         return pagingQuery
                 .createQuery(queryFactory, condition.getCursor())
                 .where(
-                        novel.deletedAt.isNull(),
-                        novel.lastEpisodeAt.goe(thisMonth)
+                        getDefaultFilterCondition(),
+                        novel.createdAt.goe(thisMonth)
                 )
                 .limit(limit)
                 .fetch();
+    }
+
+    private BooleanExpression getDefaultFilterCondition() {
+        return novel.deletedAt.isNull() // 삭제되지 않은 소설
+                .and(novel.lastEpisodeAt.isNotNull()); // 회차가 하나라도 존재하는 소설
     }
 
     private LocalDateTime getThisMonth() {
