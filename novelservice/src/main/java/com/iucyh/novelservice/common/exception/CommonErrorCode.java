3d4e704581b86e1f94dcbaf5c27cdfc40c87edbd
Jaemin <ghdckdwn8456@gmail.com>
2025-10-21 15:40:58 +0900

    [feat] MISSING_PATH_VARIABLE 오류 핸들링 코드 추가 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/common/exception/CommonErrorCode.java
+++ novelservice/src/main/java/com/iucyh/novelservice/common/exception/CommonErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-5001"),
-    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "COMMON-4001");
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "COMMON-4001"),
+    MISSING_PATH_VARIABLE(HttpStatus.BAD_REQUEST, "COMMON-4002"),
+    HTTP_MESSAGE_NOT_READABLE(HttpStatus.BAD_REQUEST, "COMMON-4003");
 
     private final HttpStatus status;
     private final String code;
--- novelservice/src/main/java/com/iucyh/novelservice/common/exception/GlobalExceptionHandler.java
+++ novelservice/src/main/java/com/iucyh/novelservice/common/exception/GlobalExceptionHandler.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.method.MethodValidationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -27,6 +29,16 @@ import java.util.Map;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
+    public ResponseEntity<FailDto> handleException(Exception e, HttpServletRequest req) {
+        log.error("{} : {}, stack trace : {}", LocalDateTime.now(), e.getMessage(), e.getStackTrace());
+
+        FailDto failDto = new FailDto(CommonErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error", req.getRequestURI(), null);
+        return ResponseEntity
+                .status(CommonErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+                .body(failDto);
+    }
+
+    @ExceptionHandler
     public ResponseEntity<FailDto> handleServiceException(ServiceException e, HttpServletRequest req) {
         log.warn("{} : {}", LocalDateTime.now(), e.getMessage());
 
@@ -36,13 +48,28 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(failDto);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<FailDto> handleException(Exception e, HttpServletRequest req) {
-        log.error("{} : {}", LocalDateTime.now(), e.getMessage());
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn("{} : {}", LocalDateTime.now(), ex.getMessage());
 
-        FailDto failDto = new FailDto(CommonErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error", req.getRequestURI(), null);
+        String path = getRequestPath(request);
+        String variableName = ex.getVariableName();
+
+        FailDto failDto = new FailDto(CommonErrorCode.MISSING_PATH_VARIABLE, "Missing path variable", path, Map.of("missingVariable", variableName));
         return ResponseEntity
-                .status(CommonErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+                .status(CommonErrorCode.MISSING_PATH_VARIABLE.getStatus())
+                .body(failDto);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn("{} : {}", LocalDateTime.now(), ex.getMessage());
+
+        String path = getRequestPath(request);
+        FailDto failDto = new FailDto(CommonErrorCode.HTTP_MESSAGE_NOT_READABLE, "Cannot read request body", path, null);
+
+        return ResponseEntity
+                .status(CommonErrorCode.HTTP_MESSAGE_NOT_READABLE.getStatus())
                 .body(failDto);
     }
 
@@ -50,10 +77,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         log.error("{} : {}", LocalDateTime.now(), ex.getMessage());
 
-        ServletWebRequest servletWebRequest = (ServletWebRequest)request;
-        String path = servletWebRequest.getRequest().getRequestURI();
-
+        String path = getRequestPath(request);
         Map<String, String> failedFields = getFailedFields(ex);
+
         FailDto failDto = new FailDto(CommonErrorCode.VALIDATION_FAILED, "Field validation failed", path, Map.of("fields", failedFields));
         return ResponseEntity
                 .status(CommonErrorCode.VALIDATION_FAILED.getStatus())
@@ -68,6 +94,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             failedFields.put(error.getField(), error.getDefaultMessage());
         });
         return failedFields;
+    }
+
+    private String getRequestPath(WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest)request;
+        return servletWebRequest.getRequest().getRequestURI();
     }
 }
 
