d3f434e7d31abccabc70cd725d42a086d15fc061
Jaemin <ghdckdwn8456@gmail.com>
2025-10-21 15:45:34 +0900

    [fix] title->빈 문자열도 거부, description->null만 거부 로 각각 수정 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/dto/novel/CreateNovelDto.java
+++ novelservice/src/main/java/com/iucyh/novelservice/dto/novel/CreateNovelDto.java
@@ -2,7 +2,7 @@ package com.iucyh.novelservice.dto.novel;
 
 import com.iucyh.novelservice.common.validator.enumfield.EnumField;
 import com.iucyh.novelservice.domain.novel.NovelCategory;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,11 +14,11 @@ import static com.iucyh.novelservice.common.constant.NovelConstants.*;
 @NoArgsConstructor
 public class CreateNovelDto {
 
-    @NotEmpty
+    @NotBlank
     @Length(min = NOVEL_TITLE_LENGTH_MIN, max = NOVEL_TITLE_LENGTH_MAX)
     private String title;
 
-    @NotEmpty
+    @NotNull
     @Length(max = NOVEL_DESC_LENGTH_MAX)
     private String description;
 
