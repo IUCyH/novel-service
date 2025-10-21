835812bb93c159e591cd0d1f06d8db483be4e8b4
Jaemin <ghdckdwn8456@gmail.com>
2025-10-21 15:39:37 +0900

    [fix] JPA와 관련없는 테스트에서 JPA Auditing 오류 나는 부분 해결 - #1
    JPA Auditing 활성화 코드 JPA 설정 클래스들로 이동

--- novelservice/src/main/java/com/iucyh/novelservice/NovelServiceApplication.java
+++ novelservice/src/main/java/com/iucyh/novelservice/NovelServiceApplication.java
@@ -2,9 +2,7 @@ package com.iucyh.novelservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class NovelServiceApplication {
 
