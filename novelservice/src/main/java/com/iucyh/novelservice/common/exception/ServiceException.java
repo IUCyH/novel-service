f81b76690c08068a50b96e970af42d705aa7f5c0
Jaemin <ghdckdwn8456@gmail.com>
2025-10-22 00:34:06 +0900

    [chore] ServiceException 생성자 다양화 - #4

--- novelservice/src/main/java/com/iucyh/novelservice/common/exception/ServiceException.java
+++ novelservice/src/main/java/com/iucyh/novelservice/common/exception/ServiceException.java
@@ -8,11 +8,23 @@ import java.util.Map;
 public abstract class ServiceException extends RuntimeException {
 
     private final ErrorCode errorCode;
-    private final Map<String, Object> causes;
+    private final Object causes;
 
-    protected ServiceException(ErrorCode errorCode, String message, Map<String, Object> causes) {
+    protected ServiceException(ErrorCode errorCode, String message, Object causes) {
         super(message);
         this.errorCode = errorCode;
         this.causes = causes;
+    }
+
+    protected ServiceException(ErrorCode errorCode) {
+        this(errorCode, errorCode.getDefaultMessage(), Map.of());
+    }
+
+    protected ServiceException(ErrorCode errorCode, Object causes) {
+        this(errorCode, errorCode.getDefaultMessage(), causes);
+    }
+
+    protected ServiceException(ErrorCode errorCode, String message) {
+        this(errorCode, message, Map.of());
     }
 }
