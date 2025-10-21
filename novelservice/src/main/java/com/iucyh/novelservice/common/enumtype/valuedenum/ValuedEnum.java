427a620ecfa522b0607d49beae0b422b84534a20
Jaemin <ghdckdwn8456@gmail.com>
2025-10-21 16:22:12 +0900

    [refactor] ValuedEnum string -> enum 변환 로직 공통화 - #1

--- novelservice/src/main/java/com/iucyh/novelservice/common/enumtype/valuedenum/ValuedEnum.java
+++ novelservice/src/main/java/com/iucyh/novelservice/common/enumtype/valuedenum/ValuedEnum.java
@@ -1,17 +1,6 @@
-package com.iucyh.novelservice.common.enumtype;
+package com.iucyh.novelservice.common.enumtype.valuedenum;
 
 public interface ValuedEnum {
 
     String getValue();
-
-    default <T extends Enum<T> & ValuedEnum> T fromValue(String value, Class<T> enumClass) {
-        if (value == null || value.isBlank()) return null;
-
-        for (T e : enumClass.getEnumConstants()) {
-            if (e.getValue().equals(value)) {
-                return e;
-            }
-        }
-        return null;
-    }
 }
