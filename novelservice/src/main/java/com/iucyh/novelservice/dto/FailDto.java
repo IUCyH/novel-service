005c7d7979fcc408b3ab878365a7e197d93688fd
Jaemin <ghdckdwn8456@gmail.com>
2025-10-22 00:56:04 +0900

    [chore] FailDto causes 필드 Object로 수정, 생성자 다양화 - #4

--- novelservice/src/main/java/com/iucyh/novelservice/dto/FailDto.java
+++ novelservice/src/main/java/com/iucyh/novelservice/dto/FailDto.java
@@ -5,6 +5,7 @@ import com.iucyh.novelservice.common.exception.ServiceException;
 import lombok.Getter;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -16,18 +17,35 @@ public class FailDto {
     private final String code;
     private final String message;
     private final String path;
-    private final Map<String, Object> causes;
+    private final Object causes;
 
     public FailDto(ErrorCode errorCode, String message, String path, Map<String, Object> causes) {
-        this.timestamp = Instant.now();
+        this.timestamp = getCurrentTimeStamp();
         this.status = errorCode.getStatus().value();
         this.code = errorCode.getCode();
         this.message = message;
         this.path = path;
-        this.causes = causes;
+        this.causes = causes == null ? Map.of() : causes;
+    }
+
+    public FailDto(ErrorCode errorCode, String message, String path, List<Object> causes) {
+        this.timestamp = getCurrentTimeStamp();
+        this.status = errorCode.getStatus().value();
+        this.code = errorCode.getCode();
+        this.message = message;
+        this.path = path;
+        this.causes = causes == null ? List.of() : causes;
+    }
+
+    public FailDto(ErrorCode errorCode, String message, String path) {
+        this(errorCode, message, path, Map.of());
     }
 
     public static FailDto from(ServiceException e, String path) {
         return new FailDto(e.getErrorCode(), e.getMessage(), path, e.getCauses());
+    }
+
+    private Instant getCurrentTimeStamp() {
+        return Instant.now();
     }
 }
