9e5d2ccd466ab4cd9f99eba29b995f5a73b15c21
Jaemin <ghdckdwn8456@gmail.com>
2025-10-26 03:54:26 +0900

    [test] 공통 Spring 에러 핸들링 테스트 코드 작성 - #4

--- novelservice/src/test/java/com/iucyh/novelservice/common/exception/testsupport/TestController.java
+++ novelservice/src/test/java/com/iucyh/novelservice/common/exception/testsupport/TestController.java
@@ -1,15 +1,25 @@
-package com.iucyh.novelservice.common.exception;
+package com.iucyh.novelservice.common.exception.testsupport;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test-con")
 public class TestController {
 
-    @GetMapping("/method-argument-type-mismatch")
-    public void methodArgumentTypeMismatch() {
+    @GetMapping("/path-variable/{variable}")
+    public ResponseEntity<Integer> pathVariable(@PathVariable Integer variable) {
+        return ResponseEntity.ok(variable);
+    }
+
+    @GetMapping("/request-parameter")
+    public ResponseEntity<String> requestParameter(@RequestParam String variable) {
+        return ResponseEntity.ok(variable);
+    }
+
+    @PostMapping("/request-body")
+    public ResponseEntity<String> requestBody(@Valid @RequestBody TestBodyDto body) {
+        return ResponseEntity.ok(body.getName());
     }
 }
