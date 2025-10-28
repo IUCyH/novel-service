f6b65207fc2ef8902e382a07791808c397e6dd86
Jaemin <ghdckdwn8456@gmail.com>
2025-10-28 11:09:41 +0900

    [test] NovelSortType->getSupportedCursorClass 반환값 검증 테스트 작성 - #4

--- novelservice/src/test/java/com/iucyh/novelservice/repository/novel/query/pagingquery/NovelPagingQueryTest.java
+++ novelservice/src/test/java/com/iucyh/novelservice/repository/novel/query/pagingquery/NovelPagingQueryTest.java
@@ -1,6 +1,7 @@
 package com.iucyh.novelservice.repository.novel.query.pagingquery;
 
 import com.iucyh.novelservice.dto.novel.NovelSortType;
+import com.iucyh.novelservice.repository.novel.query.cursor.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -41,15 +42,29 @@ public class NovelPagingQueryTest {
 
     @ParameterizedTest
     @EnumSource(NovelSortType.class)
+    @DisplayName("각 정렬 기준이 지원하는 커서 클래스가 기대값과 일치한다")
+    void everySortTypeSupportsCorrectCursorClass(NovelSortType sortType) {
+        Class<? extends NovelCursor> expectedCursor = switch (sortType) {
+            case POPULAR -> NovelPopularCursor.class;
+            case LAST_UPDATE -> NovelLastUpdateCursor.class;
+            case LIKE_COUNT -> NovelLikeCountCursor.class;
+            case VIEW_COUNT -> NovelViewCountCursor.class;
+        };
+        assertThat(sortType.getSupportedCursorClass())
+                .isEqualTo(expectedCursor);
+    }
+
+    @ParameterizedTest
+    @EnumSource(NovelSortType.class)
     @DisplayName("각 전략이 지원하는 정렬 기준이 기대값과 일치함")
-    void everyPagingQuerySupportsCorrectSortType(NovelSortType sortType) {
-        NovelPagingQuery pagingQuery = switch (sortType) {
+    void everyPagingQuerySupportsCorrectSortType(NovelSortType expectedSortType) {
+        NovelPagingQuery pagingQuery = switch (expectedSortType) {
             case POPULAR -> new NovelPopularPagingQuery();
             case LAST_UPDATE -> new NovelLastUpdatePagingQuery();
             case LIKE_COUNT -> new NovelLikeCountPagingQuery();
             case VIEW_COUNT -> new NovelViewCountPagingQuery();
         };
         assertThat(pagingQuery.getSupportedSortType())
-                .isEqualTo(sortType);
+                .isEqualTo(expectedSortType);
     }
 }
