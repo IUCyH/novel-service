6954769225c82b6fbbb1a62f282b86c50286bf3a
Jaemin <ghdckdwn8456@gmail.com>
2025-10-31 17:29:27 +0900

    [feat] 소설의 회차 중 가장 큰 회차 번호 조회 구현 - #3

--- novelservice/src/main/java/com/iucyh/novelservice/repository/episode/EpisodeRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/repository/episode/EpisodeRepository.java
@@ -2,6 +2,13 @@ package com.iucyh.novelservice.repository.episode;
 
 import com.iucyh.novelservice.domain.episode.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+
+    @Query("select max(e.episodeNumber) from Episode e where e.novel.id = :novelId")
+    Optional<Integer> findLastEpisodeNumber(@Param("novelId") long novelId);
 }
