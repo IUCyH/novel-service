c244cb28a8d884bfc3baccf3190e458d6a2b3e03
Jaemin <ghdckdwn8456@gmail.com>
2025-11-12 21:16:35 +0900

    [feat] User 엔티티 정의 - #14

---
+++ novelservice/src/main/java/com/iucyh/novelservice/user/domain/User.java
@@ -0,0 +1,39 @@
+package com.iucyh.novelservice.user.domain;
+
+import com.iucyh.novelservice.common.domain.PublicEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User extends PublicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 320, nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String nickname;
+
+    @Column(length = 100, nullable = false)
+    private String bio;
+
+    public static User of(String email, String password, String nickname, String bio) {
+        User user = new User();
+        user.email = email;
+        user.password = password;
+        user.nickname = nickname;
+        user.bio = bio;
+        return user;
+    }
+}
